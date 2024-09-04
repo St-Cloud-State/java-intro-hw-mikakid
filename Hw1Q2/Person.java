fc5f58e2600c70c81779ee4d184f64a5c28d3e44
mikakid <126606088+mikakid@users.noreply.github.com>
2024-09-04 18:58:08 +0000

    person class

---
+++ Hw1Q2/Person.java
@@ -0,0 +1,30 @@
+public class Person{
+    private String firstName;
+    private String lastName;
+    private String id;
+
+    //constructor
+    public Person(String firstName, String lastName, String id){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+    }
+
+    //Accessor methods
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    //toString() method
+    public String toString(){
+        return "First Name: " + firstName + "\n" + "Last Name: " + lastName + "\n" + "Id: " + id;
+    }
+}
